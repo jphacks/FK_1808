05b6d96adfadbc1edc3fb9895d7aeee9ef910f7d
naoto67 <uver.com67@gmail.com>
2018-10-28 05:50:49 +0900

    make reservation action.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,7 +7,19 @@ class GroupsController < ApplicationController
   end
 
   def update
-
+    @group = Group.find(params[:id])
+    if @group.book
+      flash[:alert] = "すでに予約が完了しているグループです。"
+      redirect_to root_path and return
+    end
+    date = @group.start
+    start = DateTime.new(Integer(date.year), Integer(date.month), Integer(date.day),
+                       Integer(params[:group]["start(4i)"]), Integer(params[:group]["start(5i)"]))
+    if @group.update_attributes(start: start, store_name: params[:group][:store_name],
+                               url: params[:group][:url], book: true,
+                               location: params[:group][:location])
+      redirect_to root_path
+    end
   end
 
   def groups
@@ -22,6 +34,11 @@ class GroupsController < ApplicationController
         
       }
     end
+  end
+
+  private 
+  def group_params
+    params.require(:group).permit(:store_name, :url, :location)
   end
   
 end
